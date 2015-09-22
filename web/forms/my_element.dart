4104c4c3b474a3c4666b8ced9e0fbf02a641a46d
zoechi <gzoechi@gmail.com>
2015-09-22 12:12:56 +0200

    init

---
+++ web/forms/my_element.dart
@@ -0,0 +1,11 @@
+@HtmlImport('my_element.html')
+library my_element;
+
+import 'package:web_components/web_components.dart' show HtmlImport;
+import 'package:polymer/polymer.dart';
+
+@PolymerRegister('my-element')
+class MyElement extends PolymerElement {
+  @property bool checked = false;
+  MyElement.created() : super.created();
+}
